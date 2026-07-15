0c52cd7530ed28c5111485a0dddb90c051bcb50e
HarishSRM2025 <harishsrm25@gmail.com>
2026-07-15 12:29:58 +0530

    latest

--- config/connectDB.js
+++ config/connectDB.js
@@ -1,13 +1,24 @@
 const mongoose = require('mongoose')
+const path = require('path')
+const dotenv = require('dotenv')
 
-const connectDataBase = ()=>{
-    mongoose.connect(`${process.env.DB_URL}`)
-    .then(()=>{
-        console.log('Database Connected')
-    })
-    .catch((err)=>{
-        console.error('Error connecting to database:', err)
-    })
+dotenv.config({ path: path.resolve(__dirname, '..', '.env') })
+
+const connectDataBase = () => {
+    const dbUrl = process.env.DB_URL
+
+    if (!dbUrl) {
+        console.error('DB_URL is not defined. Check backend/.env')
+        return
+    }
+
+    mongoose.connect(dbUrl, { serverSelectionTimeoutMS: 10000 })
+        .then(() => {
+            console.log('Database Connected')
+        })
+        .catch((err) => {
+            console.error('Error connecting to database:', err)
+        })
 }
 
-module.exports= connectDataBase
+module.exports = connectDataBase
